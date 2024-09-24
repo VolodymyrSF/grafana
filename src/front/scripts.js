0b7542bd71d64f2caeb4927f9c6a2703a3b5aaa4
vsf <safjanik.volody@gmail.com>
2024-09-24 14:20:36 +0300

    final v.1

--- src/front/scripts.js
+++ src/front/scripts.js
@@ -47,6 +47,7 @@ function updateTable(logs) {
     <thead>
       <tr>
         <th></th>
+        <th></th>
         <th>${stationNames[90]}</th>
         <th>${stationNames[91]}</th>
       </tr>
@@ -58,41 +59,49 @@ function updateTable(logs) {
   const legend = `
      <tbody>
         <tr>
+          <td class="legend">Час останнього оновлення</td>
           <td class="legend">Time</td>
           <td class="data-cell">${formatTime(logs.find(log => log.diris_id === 90)?.time || '')}</td>
           <td class="data-cell">${formatTime(logs.find(log => log.diris_id === 91)?.time || '')}</td>
         </tr>
         <tr>
+          <td class="legend">Активна потужність</td>
           <td class="legend">P, кВт</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.pa_p }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.pa_p }</td>
         </tr>
         <tr>
+          <td class="legend">Реактивна потужність</td>
           <td class="legend">Q, квар</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.pa_r }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.pa_r }</td>
         </tr>
          <tr>
+          <td class="legend">Повна потужність</td>
           <td class="legend">S, кВА</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.pa_s }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.pa_s }</td>
         </tr>
         <tr>
+          <td class="legend">Коеціфієнт потужності</td>
           <td class="legend">cos φ</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.pa }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.pa }</td>
         </tr>
         <tr>
+          <td class="legend">Струм фаза А</td>
           <td class="legend">IL1, А</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.i1 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.i1 }</td>
         </tr>
         <tr>
+          <td class="legend">Струм фаза В</td>
           <td class="legend">IL2, А</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.i2 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.i2 }</td>
         </tr>
         <tr>
+          <td class="legend">Струм фаза С</td>
           <td class="legend">IL3, А</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.i3 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.i3 }</td>
@@ -100,21 +109,25 @@ function updateTable(logs) {
         
         
         <tr>
+          <td class="legend">Міжфазна напруга А</td>
           <td class="legend">U12, В</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.u12 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.u12 }</td>
         </tr>
         <tr>
+          <td class="legend">Міжфазна напруга В</td>
           <td class="legend">U23, В</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.u23 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.u23 }</td>
         </tr>
         <tr>
+          <td class="legend">Міжфазна напруга С</td>
           <td class="legend">U31, В</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.u31 }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.u31 }</td>
         </tr>
         <tr>
+          <td class="legend">Частота</td>
           <td class="legend">F, Гц</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 90)?.f }</td>
           <td class="data-cell">${logs.find(log => log.diris_id === 91)?.f }</td>
